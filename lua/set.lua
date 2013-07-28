e9f9ffa2ec589ff462ccde7e2bd8b77db715a05c
yihua.huang <code4crafter@gmail.com>
2013-07-28 14:58:23 +0800

    set url

--- lua/set.lua
+++ lua/set.lua
@@ -6,49 +6,66 @@
 -- lua redis module https://github.com/agentzh/lua-resty-redis
 -- http luad module http://wiki.nginx.org/HttpLuaModule
 
-local redis = require "resty.redis"
+local redis = require"resty.redis"
 local red = redis:new()
 local server = "127.0.0.1"
+local host = "http://127.0.0.1/"
 
 --connect
 red:set_timeout(1000) -- 1 sec
 local ok, err = red:connect(server, 6379)
 if not ok then
-    ngx.log(ngx.INFO,"failed to connect: ",err)
-    ngx.say("failed to connect: ",err)
+    ngx.log(ngx.INFO, "failed to connect: ", err)
+    ngx.say("failed to connect: ", err)
     return
 end
 
 --ngx.log(ngx.req.get_uri_args())
 --ngx.say(ngx.req.get_uri_args(1))
 
-length=7
+length = 7
 
 function short(url)
-    local shortUrl = string.sub(ngx.md5(url), 0, length)
+    local shortUrl = ngx.md5(url):sub(0, length)
     return shortUrl
 end
 
 local args = ngx.req.get_uri_args()
 if not args["url"] then
+    ngx.status = 400
     ngx.say("Please specific url!")
     return
 end
 
-local url=args["url"]
-if not string.match(url,"^http[s]?://") then
+local url = args["url"]
+if not url:match("^http[s]?://") then
+    ngx.status = 400
     ngx.say("Unsupport url!")
     return
 end
 
-local shortUrl = short(url)
+local shortUrl
+local tempUrl = url
+while not shortUrl do
+    shortUrl = short(tempUrl)
+    local res, err = red:get(shortUrl)
+    if res==ngx.null then
+        red:set(shortUrl, url)
+        break
+    elseif res==url then
+        break
+    end
+    tempUrl = tempUrl .. "X"
+    shortUrl = nil
+end
 
-ngx.say(shortUrl)
+ngx.status = 200
+ngx.say(host..shortUrl)
 
 -- keepalive
-local ok,err = red:set_keepalive(0,100)
-if  ok then
-    ngx.log(ngx.INFO,"Sucessfully keepalive",err)
+local ok, err = red:set_keepalive(0, 100)
+if ok then
+    ngx.log(ngx.INFO, "Sucessfully keepalive", err)
 else
-    ngx.log(ngx.ERR,"Failed to keepalive",err)
+    ngx.log(ngx.ERR, "Failed to keepalive", err)
 end
