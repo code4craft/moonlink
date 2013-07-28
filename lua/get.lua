1906d88e5c19f55a59861ed3ab995b604b6a9868
yihua.huang <code4crafter@gmail.com>
2013-07-28 13:19:39 +0800

    add redis conntect

--- lua/get.lua
+++ lua/get.lua
@@ -4,4 +4,24 @@
 -- Time: 下午12:31
 --
 
-ngx.say("get")
+local redis = require "resty.redis"
+local red = redis:new()
+local server = "127.0.0.1"
+
+--connect
+red:set_timeout(1000) -- 1 sec
+local ok, err = red:connect(server, 6379)
+if not ok then
+    ngx.log(ngx.INFO,"failed to connect: ",err)
+    ngx.say("failed to connect: ",err)
+    return
+end
+
+
+-- keepalive
+local ok,err = red:set_keepalive(0,100)
+if  ok then
+    ngx.log(ngx.INFO,"Sucessfully keepalive",err)
+else
+    ngx.log(ngx.ERR,"Failed to keepalive",err)
+end
--- lua/set.lua
+++ lua/set.lua
@@ -3,5 +3,24 @@
 -- Date: 13-7-28
 -- Time: 下午12:15
 --
+local redis = require "resty.redis"
+local red = redis:new()
+local server = "127.0.0.1"
 
-ngx.say("set")
+--connect
+red:set_timeout(1000) -- 1 sec
+local ok, err = red:connect(server, 6379)
+if not ok then
+    ngx.log(ngx.INFO,"failed to connect: ",err)
+    ngx.say("failed to connect: ",err)
+    return
+end
+
+
+-- keepalive
+local ok,err = red:set_keepalive(0,100)
+if  ok then
+    ngx.log(ngx.INFO,"Sucessfully keepalive",err)
+else
+    ngx.log(ngx.ERR,"Failed to keepalive",err)
+end
